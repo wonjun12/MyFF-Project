2dcf3f702a575d58011ec73d85045dc89f723c75
wonjun12 <108748094+wonjun12@users.noreply.github.com>
2022-10-08 18:30:57 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import express from "express";
-// import models from "./models"
 import homeRouter from "./Router/homeRouter";
 import userRouter from "./Router/userRouter";
 import boardRouter from "./Router/boardRouter";
 
 const app = express();
 const port = 3000;
-
-// models.sequelize.sync().then(() => console.log("연결 성공")).catch(() => console.log("연결 실패"));
 
 app.use(express.urlencoded({extended: true}));
 
