5c1b997e29ec0c603b68aa88741abbec3827cc29
wonjun12 <108748094+wonjun12@users.noreply.github.com>
2022-10-11 02:35:02 +0900

    Update boardController.js

--- src/Controller/boardController.js
+++ src/Controller/boardController.js
@@ -1,7 +1,7 @@
 import models from "../models";
 
 //사진 넣기 함수
-async function addPicture(BID, Poto){
+async function createPicture(BID, Poto){
     await models.Picture.create({
         BID: BID,
         Poto: Poto
@@ -15,6 +15,13 @@ async function updatePicture(PID, Poto){
    },{
         where: {PID: PID}
    })
+}
+
+//사진 삭제 함수
+async function deletePicture(PID) {
+    await models.Picture.destroy({
+        where: {PID: PID}
+    })
 }
 
 //게시판 작성 html
@@ -99,19 +106,50 @@ export const boardEditPost = async (req, res) => {
         where: {BID: id}
     })
 
+    //사진 배열 여부 확인 (2개 이상일 경우만 배열로 적용됨)
+    const arrayCheck = Array.isArray(writeImgName);
     //해당 게시글 사진 조회
     await models.Picture.findAll({
         where: {BID: id}
     }).then(data => {
-        //조회한 값들 중에서 순서대로 사진 값 수정
-        if(Array.isArray(writeImgName)){
-            //사진 입력이 여러개 일경우 실행
-            for(let poto in writeImgName){
-                updatePicture(data[poto].PID, writeImgName[poto]);
+        //사진 수정
+        if(arrayCheck){
+            const MAX_DATA = data.length;
+            const MAX_IMG = writeImgName.length;
+            //DB 사진 갯수와 입력한 사진 갯수 비교
+            if(MAX_IMG > MAX_DATA){
+                for(let i in writeImgName){
+                    if(i >= MAX_DATA){
+                        //넣은 사진이 많다면 사진을 추가함
+                        createPicture(id, writeImgName[i]);
+                    }else {
+                        //넣은 사진 까지는 사진 수정
+                        updatePicture(data[i].PID, writeImgName[i]);
+                    }
+                }
+            }else {
+                for(let i in data){
+                    if(i >= MAX_IMG){
+                        //넣은 사진이 DB의 갯수보다 적다면 삭제
+                        deletePicture(data[i].PID);
+                    }else {
+                        //넣은 사진 만큼 수정
+                        //DB갯수와 넣은 사진의 갯수가 동일할 경우 모두 수정됨
+                        updatePicture(data[i].PID, writeImgName[i]);
+                    }
+                }
             }
         }else {
-            //사진이 한개일때 실행
-            updatePicture(data[0].PID, writeImgName);
+            //넣은 사진이 하나일 경우
+            for(let i in data){
+                if(i > 0){
+                    //DB에 저장된게 2개 이상일경우 전부 삭제
+                    deletePicture(data[i].PID);
+                }else{
+                    //한개의 사진만 수정
+                    updatePicture(data[i].PID, writeImgName);
+                }
+            }
         }
     })
 
